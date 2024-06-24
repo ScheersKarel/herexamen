537c8961321bf7a6de47acf400a6fcf4bc9a3f7b
ScheersKarel <karel.scheers@student.kdg.be>
2024-06-24 11:46:02 +0200

    fixed imageminify in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,38 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+var uglify = require('gulp-uglify');
+var cleaner = require('gulp-clean');
 const browserSync = require('browser-sync').create();
 
-// Compile SCSS to CSS
+//SCSS naar CSS
 function buildStyles() {
-  return gulp.src('./src/scss/**/*.scss')
+  return gulp.src('./src/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./public/resources/css/'))
     .pipe(browserSync.stream());
-}
+};
 
-// Optimize images
-async function images() {
-  const imagemin = (await import('gulp-imagemin')).default;
-  return gulp.src('./src/images/**/*')
+//optimize images 
+function images() {
+  return gulp.src('./src/images/*')
     .pipe(imagemin())
     .pipe(gulp.dest('./public/resources/images/'))
     .pipe(browserSync.stream());
 }
 
-// Compress JavaScript
 function compress() {
-  return gulp.src('./src/js/**/*.js')
+  return gulp.src('./src/js/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('./public/resources/js/'))
     .pipe(browserSync.stream());
 }
 
-// Clean output directory
-async function clean() {
-  const del = (await import('del')).deleteAsync;
-  return del(['./public/resources/*']);
+function clean() {
+  return gulp.src('./public/resources/*', { read: false })
+    .pipe(cleaner());
 }
 
-// Watch files and reload with BrowserSync
 function browserSyncTask() {
   browserSync.init({
     server: {
@@ -42,17 +40,14 @@ function browserSyncTask() {
     }
   });
   gulp.watch('./src/scss/**/*.scss', buildStyles);
-  gulp.watch('./src/images/**/*', images);
-  gulp.watch('./src/js/**/*.js', compress);
-  gulp.watch('./public/**/*.php').on('change', browserSync.reload);
-  gulp.watch('./public/**/*.html').on('change', browserSync.reload);
+  gulp.watch('./src/images/*', images);
+  gulp.watch('./src/js/*', compress);
+  gulp.watch('./public/*.php').on('change', browserSync.reload);
+  gulp.watch('./public/*.html').on('change', browserSync.reload);
+  buildStyles();
+  images();
+  compress();
 }
-
-// Define complex tasks
-const build = gulp.series(clean, gulp.parallel(buildStyles, images, compress));
-const watch = gulp.series(build, browserSyncTask);
-
-// Export tasks
 exports.clean = clean;
-exports.build = build;
-exports.default = watch;
+exports.dev = browserSyncTask;
+exports.build = gulp.series(clean, buildStyles, images, compress);
