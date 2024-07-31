06a75f7b1b84ae95647d2dea623f601e1d5cd808
ScheersKarel <karel.scheers@student.kdg.be>
2024-07-31 15:13:30 +0200

    achievement page done

--- public/badges.php
+++ public/badges.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>rewards</title>
+    <title>Badges</title>
     <link rel="stylesheet" href="./resources/css/main.css">
 </head>
 
@@ -29,7 +29,68 @@
     <div class="container">
 
         <div class="achievementCard">
+            <div class="achievementCard__image">
+                <img class="achievementCard__image__img" src="./resources/images/badge1.png" alt="">
+                <p class="achievementCard__image__text">Level 1</p>
+            </div>
+            <div class="achievementCard__content">
+                <p class="achievementCard__content__title">Festival groentje</p>
+                <p class="achievementCard__content__description">Vul 1 enquête in <span class="achievementCard__content__description__number"><b>1</b>/1</span></p>
+                <div class="achievementCard__content__progressbar"></div>
+                <div class="achievementCard__content__progressbarFilled"></div>
+            </div>
+        </div>
 
+        <div class="achievementCard">
+            <div class="achievementCard__image">
+                <img class="achievementCard__image__img" src="./resources/images/badge2small.png" alt="">
+                <p class="achievementCard__image__text">Level 2</p>
+            </div>
+            <div class="achievementCard__content">
+                <p class="achievementCard__content__title">Enquête ontdekker</p>
+                <p class="achievementCard__content__description">Vul in totaal 3 enquête in <span class="achievementCard__content__description__number"><b>3</b>/3</span></p>
+                <div class="achievementCard__content__progressbar"></div>
+                <div class="achievementCard__content__progressbarFilled"></div>
+            </div>
+        </div>
+
+        <div class="achievementCard">
+            <div class="achievementCard__image">
+                <img class="achievementCard__image__img__lock" src="./resources/images/badgeLock.png" alt="">
+                <p class="achievementCard__image__text">Level 3</p>
+            </div>
+            <div class="achievementCard__content">
+                <p class="achievementCard__content__title">Menigte bijdrager</p>
+                <p class="achievementCard__content__description">Vul een reeks van 5 enquête in <span class="achievementCard__content__description__number"><b>2</b>/5</span></p>
+                <div class="achievementCard__content__progressbar"></div>
+                <div class="achievementCard__content__progressbar25"></div>
+            </div>
+        </div>
+
+        <div class="achievementCard">
+            <div class="achievementCard__image">
+                <img class="achievementCard__image__img__lock" src="./resources/images/badgeLock.png" alt="">
+                <p class="achievementCard__image__text">Level 4</p>
+            </div>
+            <div class="achievementCard__content">
+                <p class="achievementCard__content__title">Podium specialist</p>
+                <p class="achievementCard__content__description">Vul in totaal 10 enquête in <span class="achievementCard__content__description__number"><b>3</b>/10</span></p>
+                <div class="achievementCard__content__progressbar"></div>
+                <div class="achievementCard__content__progressbar310"></div>
+            </div>
+        </div>
+
+        <div class="achievementCard">
+            <div class="achievementCard__image">
+                <img class="achievementCard__image__img__lock" src="./resources/images/badgeLock.png" alt="">
+                <p class="achievementCard__image__text">Level 5</p>
+            </div>
+            <div class="achievementCard__content">
+                <p class="achievementCard__content__title">Streak meester</p>
+                <p class="achievementCard__content__description">Vul een reeks van 10 enquête in <span class="achievementCard__content__description__number"><b>2</b>/10</span></p>
+                <div class="achievementCard__content__progressbar"></div>
+                <div class="achievementCard__content__progressbar15"></div>
+            </div>
         </div>
 
     </div>
