29ac80438fb5c5594a83fd07b7ec55e1ebffcecd
ScheersKarel <karel.scheers@student.kdg.be>
2024-08-15 13:19:59 +0200

    podiumdrukte page finished

--- public/podiumdrukte.php
+++ public/podiumdrukte.php
@@ -18,6 +18,54 @@
             <button class="dropdown__button">Mainstage</button>
             <img class="dropdown__arrow" src="./resources/images/downArrow.png" alt="">
         </div>
+
+        <div class="crowd">
+            <div class="crowd__amount">
+                <div class="crowd__amount__1"></div>
+                <div class="crowd__amount__2"></div>
+                <div class="crowd__amount__3"></div>
+                <div class="crowd__amount__4"></div>
+                <div class="crowd__amount__5"></div>
+                <div class="crowd__amount__6"></div>
+                <div class="crowd__amount__7"></div>
+                <div class="crowd__amount__8"></div>
+                <div class="crowd__amount__9"></div>
+                <div class="crowd__amount__10"></div>
+                <div class="crowd__amount__11"></div>
+                <div class="crowd__amount__12"></div>
+                <div class="crowd__amount__13"></div>
+                <div class="crowd__amount__14"></div>
+                <div class="crowd__amount__15"></div>
+                <div class="crowd__amount__16"></div>
+
+            </div>
+
+            <div class="crowd__time">
+                <div class="crowd__time__18u">
+                    <div class="crowd__time__progressbarFull"></div>
+                    <p>18u</p>
+                </div>
+                <div class="crowd__time__19u">
+                    <div class="crowd__time__progressbarFull"></div>
+                    <p>19u</p>
+                </div>
+                <div class="crowd__time__20u">
+                    <div class="crowd__time__progressbarFull"></div>
+                    <p>20u</p>
+                </div>
+                <div class="crowd__time__now">
+                    <div class="crowd__time__progressbar"></div>
+                    <div class="crowd__time__progressbar80"></div>
+                    <p>Nu</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="wrongInfo">
+            <h3 class="wrongInfo__title">Klopt deze informatie niet?</h3>
+            <p class="wrongInfo__content">Je kan de drukte aan dit podium handmatig ingeven zodat onze grafiek weer klopt.</p>
+            <a class="wrongInfo__link" href="./changeCrowd.php">Ga erheen >></a>
+        </div>
     </div>
     <?php
     include('./components/footer.php');
