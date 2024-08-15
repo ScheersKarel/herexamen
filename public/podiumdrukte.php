cff7497cab116c480ce9200d3461701f6c8cbfda
ScheersKarel <karel.scheers@student.kdg.be>
2024-08-15 12:24:12 +0200

    made header podiumdrukte page

---
+++ public/podiumdrukte.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Podiumdrukte</title>
+    <link rel="stylesheet" href="./resources/css/main.css">
+</head>
+
+<body>
+    <div class="container">
+        <img class="logo" src="resources/images/logo.png">
+
+        <h2>Live drukte aan podia</h2>
+
+        <div class="dropdown">
+            <button class="dropdown__button">Mainstage</button>
+            <img class="dropdown__arrow" src="./resources/images/downArrow.png" alt="">
+        </div>
+    </div>
+    <?php
+    include('./components/footer.php');
+    ?>
+</body>
+
+</html>
